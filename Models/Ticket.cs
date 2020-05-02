47a5656fafe7569660cb0d0d1ca2ca2786ebbc3c
CatalinVasilache <CatalinVasilache@users.noreply.github.com>
2020-05-02 11:54:22 +0300

    Add project files.

---
+++ Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComunitateaMea.Models
+{
+    public class Ticket
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime PublishedDate { get; set; }
+        public int Votes { get; set; }
+    }
+}
