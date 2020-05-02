9bfbd56a0b3afc5fd762d7253e53d4455e1a4273
CatalinVasilache <CatalinVasilache@users.noreply.github.com>
2020-05-02 12:45:37 +0300

    create header for tickets

---
+++ Data/Migrations/20200502090932_AddTicketModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ComunitateaMea.Data.Migrations
+{
+    public partial class AddTicketModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ticket",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    PublishedDate = table.Column<DateTime>(nullable: false),
+                    Votes = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ticket", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ticket");
+        }
+    }
+}
