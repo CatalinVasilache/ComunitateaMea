dc8d4b0d8f94addcb32e8b376cce49fde4af18fe
CatalinVasilache <CatalinVasilache@users.noreply.github.com>
2020-06-07 13:03:59 +0300

    Customize user model

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace ComunitateaMea.Models
 {
-    public class AppUser
+    public class AppUser : IdentityUser
     {
+        public County County { get; set; }
+
+        public int Age { get; set; }
+
+        [Required]
+        public string Salary { get; set; }
+    }
+    public enum County
+    {
+        Galati,
+        Ilfov,
+        Braila,
+        Alba,
+        Brasov
     }
 }
